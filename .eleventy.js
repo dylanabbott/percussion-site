b4ad70cd07371761fda7488b60f1cfb3edd23c61
dylanabbott <dylanabbott@gmail.com>
2021-12-10 15:39:23 -0800

    Add Forestry.io admin to output

--- .eleventy.js
+++ .eleventy.js
@@ -2,7 +2,7 @@ module.exports = (eleventyConfig) => {
     eleventyConfig.addPassthroughCopy('./src/style/style.css');
     eleventyConfig.addPassthroughCopy('./src/style/style.css.map');
     eleventyConfig.addPassthroughCopy('./src/js/*');
-    
+    eleventyConfig.addPassthroughCopy('./src/admin/*');
     return {
         markdownTemplateEngine: "njk",
         htmlTemplateEngine: "njk",
