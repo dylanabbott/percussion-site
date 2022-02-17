8c3637516dd42c76dc86616d031a57d88ee5aec2
dylanabbott <dylanabbott@gmail.com>
2022-02-17 12:19:11 -0800

    Add member data and create sorted collection

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,5 @@
 module.exports = (eleventyConfig) => {
+    // Pass through static assets and bundled CSS
     eleventyConfig.addPassthroughCopy('./src/style/style.css');
     eleventyConfig.addPassthroughCopy('./src/style/style.css.map');
     eleventyConfig.addPassthroughCopy('./src/js/*');
@@ -6,6 +7,14 @@ module.exports = (eleventyConfig) => {
     eleventyConfig.addPassthroughCopy('./src/admin');
     eleventyConfig.addPassthroughCopy('./src/.forestry');
     eleventyConfig.addPassthroughCopy('./uploads')
+
+    // Add sorted collection of members
+    eleventyConfig.addCollection("membersSorted", function(collectionApi) {
+        return collectionApi.getFilteredByTag("members").sort(function(a, b) {
+            return a.data.order - b.data.order;
+        });
+    });
+
     return {
         markdownTemplateEngine: "njk",
         htmlTemplateEngine: "njk",
