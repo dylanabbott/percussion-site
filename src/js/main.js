a1a914274ca83eb7e0875257cffda79a478a0075
dylanabbott <dylanabbott@gmail.com>
2022-03-30 17:42:41 -0700

    Update contact settings

--- src/js/main.js
+++ src/js/main.js
@@ -117,15 +117,15 @@ function submitForm(e) {
 		let formData = new FormData(contactForm);
 		formData.append('_replyto', formData.get('email'));
 		let formSubmission = Object.fromEntries(formData.entries());
-		//console.log(formSubmission);
+		// console.log(formSubmission);
 
-		// fetch('https://formsubmit.co/ajax/72a94a8f26281b097d6ba54ad8887041', {
-		// 	method: 'POST',
-		// 	body: JSON.stringify(formSubmission),
-		// })
-		// 	.then((response) => response.json())
-		// 	.then((data) => console.log(data))
-		// 	.catch((error) => console.log(error));
+		fetch('https://formsubmit.co/ajax/team@percussionstrategic.com', {
+			method: 'POST',
+			body: JSON.stringify(formSubmission),
+		})
+			.then((response) => response.json())
+			.then((data) => console.log(data))
+			.catch((error) => console.log(error));
 
 		confirmSubmission();
 	}
