1419d3ec991c247afe5f81786380f2e059b1d709
dylanabbott <dylanabbott@gmail.com>
2023-06-21 16:09:55 -0700

    Update blog and case layouts

--- src/_data/blog.js
+++ src/_data/blog.js
@@ -3,7 +3,7 @@ const EleventyFetch = require('@11ty/eleventy-fetch');
 let PROJECT_ID = 'irv9pire';
 let DATASET = 'production';
 let QUERY = encodeURIComponent(
-	'*[_type == "blog"] | order(postedDate desc) { title, subtitle, author, tags, pullQuote, "imageUrl": featuredImage.asset->url, content[]{..., asset->}}'
+	'*[_type == "blog"] | order(postedDate desc) { title, subtitle, author->{name}, tags, pullQuote, "imageUrl": featuredImage.asset->url, featuredImage, content[]{..., asset->}}'
 );
 let URL = `https://${PROJECT_ID}.api.sanity.io/v2021-10-21/data/query/${DATASET}?query=${QUERY}`;
 
