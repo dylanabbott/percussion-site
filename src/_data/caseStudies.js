db86bbff5a15172d147999bb24f060d85f2773e3
dylanabbott <dylanabbott@gmail.com>
2023-06-27 13:31:46 -0700

    Update Case Studies Layout

--- src/_data/caseStudies.js
+++ src/_data/caseStudies.js
@@ -3,14 +3,14 @@ const EleventyFetch = require('@11ty/eleventy-fetch');
 let PROJECT_ID = 'irv9pire';
 let DATASET = 'production';
 let QUERY = encodeURIComponent(
-	'*[_type == "caseStudies"] | order(postedDate desc) { title, "featuredImageUrl": featuredImage.asset->url, problem, solution, tactics[]{..., "iconUrl": icon.asset->url}, results[]{..., "iconUrl": icon.asset->url, "iconText": iconText->}, conclusion}'
+	'*[_type == "caseStudies"] | order(postedDate desc) { title, "featuredImageUrl": featuredImage.asset->url, problem, solution, tactics[]{..., "iconUrl": icon.asset->url, "iconCaption": caption}, results[]{..., "iconUrl": icon.asset->url, "iconCaption": caption}, conclusion}'
 );
 let URL = `https://${PROJECT_ID}.api.sanity.io/v2021-10-21/data/query/${DATASET}?query=${QUERY}`;
 
 module.exports = async function () {
 	return EleventyFetch(URL, {
 		directory: '.cache',
-		duration: '1d',
+		duration: '0s',
 		type: 'json',
 	});
 };
